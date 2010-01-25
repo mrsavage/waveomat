e84de2a9b634322a912366c69355f459a81e82cf
locher.hanspeter <locher.hanspeter@6dab4d5e-099c-11df-b317-0f3410dc57f6>
2010-01-25 13:10:05 +0000

    add encoding spec, change bot to post funny german sentence's (understandable for insiders only)

--- trunk/trunk/waveomat.py
+++ trunk/trunk/waveomat.py
@@ -1,27 +1,44 @@
+# -*- coding: utf-8 -*-
+
 from waveapi import events
 from waveapi import model
 from waveapi import robot
 
+from random import randint
+
+TEXTS = [u"Da krieg ich Puls!",
+         u"Geht nich, ich hab gleich Telko.",
+         u"Ist der Rundel da?",
+         u"Ich kann so nicht arbeiten...",
+         u"Deine Leistung könnte besser"]
+
+
 def OnParticipantsChanged(properties, context):
-  """Invoked when any participants have been added/removed."""
-  added = properties['participantsAdded']
-  for p in added:
-    Notify(context)
+    """Invoked when any participants have been added/removed."""
+    added = properties['participantsAdded']
+    for p in added:
+        Notify(context)
 
 def OnRobotAdded(properties, context):
-  """Invoked when the robot has been added."""
-  root_wavelet = context.GetRootWavelet()
-  root_wavelet.CreateBlip().GetDocument().SetText("I'm alive!")
+    """Invoked when the robot has been added."""
+    Notify(context)
+
+def OnDocumentChanged(properties, context):
+    """Invoked when the document has been changed."""
+    if randint(0,1):
+        text = TEXTS[randint(0,len(TEXTS))]
+        Notify(text)
 
 def Notify(context):
-  root_wavelet = context.GetRootWavelet()
-  root_wavelet.CreateBlip().GetDocument().SetText("Hi everybody!")
+    text = TEXTS[randint(0,len(TEXTS))]
+    root_wavelet = context.GetRootWavelet()
+    root_wavelet.CreateBlip().GetDocument().SetText(text)
 
 if __name__ == '__main__':
-  myRobot = robot.Robot('waveomat', 
-      image_url='http://waveomat.appspot.com/icon.png',
-      version='1',
-      profile_url='http://waveomat.appspot.com/')
-  myRobot.RegisterHandler(events.WAVELET_PARTICIPANTS_CHANGED, OnParticipantsChanged)
-  myRobot.RegisterHandler(events.WAVELET_SELF_ADDED, OnRobotAdded)
-  myRobot.Run()
+    myRobot = robot.Robot('waveomat', 
+        image_url='http://waveomat.appspot.com/icon.png',
+        version='4',
+        profile_url='http://waveomat.appspot.com/')
+    myRobot.RegisterHandler(events.WAVELET_PARTICIPANTS_CHANGED, OnParticipantsChanged)
+    myRobot.RegisterHandler(events.WAVELET_SELF_ADDED, OnRobotAdded)
+    myRobot.Run()
