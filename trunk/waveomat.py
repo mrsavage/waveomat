afb8364cbbd72af16f4c889bd4662cc3ff41be5d
locher.hanspeter <locher.hanspeter@6dab4d5e-099c-11df-b317-0f3410dc57f6>
2010-01-26 14:21:05 +0000

    added a handler for /, and a Information page

--- trunk/waveomat.py
+++ trunk/waveomat.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     logging.debug("waveomat.py __main__")
     myRobot = WaveomatRobot('waveomat', 
         image_url='http://waveomat.appspot.com/icon.png',
-        version='13',
+        version='14',
         profile_url='http://waveomat.appspot.com/')
     myRobot.RegisterHandler(events.WAVELET_PARTICIPANTS_CHANGED, OnParticipantsChanged)
     myRobot.RegisterHandler(events.WAVELET_SELF_ADDED, OnRobotAdded)
--- trunk/web.py
+++ trunk/web.py
@@ -28,7 +28,7 @@ from google.appengine.ext import webapp
 from robot import TEXTS
 
 class RobotExternalHandler(webapp.RequestHandler):
-  """Handler for serving extertnal requests"""
+  """Handler for serving external requests"""
 
   def __init__(self, robot):
     """Initializes this handler with a specific robot."""
@@ -44,6 +44,49 @@ class RobotExternalHandler(webapp.RequestHandler):
     print ''
     print TEXTS
 
+class RobotFrontPageHandler(webapp.RequestHandler):
+  """Handler for frontpage"""
 
+  def __init__(self, robot):
+    """Initializes this handler with a specific robot."""
+    self._robot = robot
+
+  def get(self):
+    """Handles HTTP GET request."""
+    html = """
+              <html>
+                <body>
+                    <h1>waveomat</h1>
+                    <p>Welcome to the waveomat, a robot for 
+                    <a href="https://wave.google.com/wave/">Google Wave</a>
+                    </p>
+                    <h2>Usage:</h2>
+                    <p>waveomat blogs into your wave whenever a 
+                    <a href="http://google.about.com/od/b/g/google_wave_blip.htm">blip</a>
+                    is submitted.
+                    </p>
+                    <p>To add waveomat to your wave, invite <b>waveomat@appspot.com</b> 
+                    to your wave.
+                    </p>
+                    <p>You can change what waveomat says using this url:</p>
+                    <pre>
+                       http://waveomat.appspot.com/_wave/text?text=whateveryouwant
+                    </pre>
+                    <p>Currently, he says:</p>
+                    <pre>
+                    %s
+                    </pre>
+                    <h2>Who? How?</h2>
+                    <p>waveomat was implemented at the <a href="http://code.google.com/p/snowsprint2010/">Snowsprint 2010</a>
+                    <p>The code lives <a href="http://code.google.com/p/waveomat/">here</a>.
+                    <p>Contributors:</p>
+                    <ul>
+                        <li>Hans-Peter Locher (Author) <a href="http://www.inquant.de">InQuant GmbH</a></li>
+                    </ul>
+                  </form>
+                </body>
+              </html>""" % TEXTS[-1]
+
+    self.response.out.write(html)
 
 # vim: set ft=python ts=4 sw=4 expandtab :
